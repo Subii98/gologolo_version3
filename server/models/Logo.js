fe68b229a434a91c9b14f5f374ede0f9734ac8ab
SubinPark111820841 <Subin.Park.1@stonybrook.edu>
2020-05-10 22:46:24 +0900

    Final version of gologolo - changed option for creating

--- server/models/Logo.js
+++ server/models/Logo.js
@@ -3,15 +3,16 @@ var mongoose = require('mongoose');
 //add all properties of logo
 var LogoSchema = new mongoose.Schema({
   id: String,
-  text: String,
+  //text: String,
+  text:{type: String, minlength:1, trim:true},
   color: String,
-  fontSize: { type: Number, min: 2, max: 144 },
+  fontSize: { type: Number, min: 1, max: 144 },
   backgroundColor: String,
   borderColor:String,
-  borderRadius:Number,
-  borderWidth:Number,
-  padding:Number,
-  margin: { type: Number, min: 2, max: 144 },
+  borderRadius:{ type: Number, min: 1, max: 144 },
+  borderWidth:{ type: Number, min: 1, max: 144 },
+  padding:{ type: Number, min: 1, max: 144 },
+  margin: { type: Number, min: 1, max: 144 },
   lastUpdate: { type: Date, default: Date.now }
 });
 
